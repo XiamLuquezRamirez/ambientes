7ee0397c775e538e949edfa0552fcf508eb5f861
Fabian Quintero <fabian.quintero.2201@gmail.com>
2026-08-14 09:51:53 -0500

    Updated navigation link in the conditions partial view from a button to an anchor tag for improved accessibility and semantic HTML structure.

--- resources/views/partials/nav-link-condiciones.blade.php
+++ resources/views/partials/nav-link-condiciones.blade.php
@@ -1,5 +1,5 @@
 <li class="nav-item">
-    <button type="button" class="nav-link border-0 bg-transparent w-100 text-start" data-abrir-modal-condiciones>
+    <a type="button" class="nav-link " data-abrir-modal-condiciones>
         <i class="fa-solid fa-book-medical"></i> Condiciones
-    </button>
+    </a>
 </li>
